eef550230842f90681085892e4022f0b0173a409
Max Baldwin <maxrbaldwin@gmail.com>
2016-10-10 16:29:23 -0500

    push before class

--- CarLot.js
+++ CarLot.js
@@ -1,14 +1,33 @@
-var CarLot = (function () {
-  var inventory = [];
+var CarLot = (function() {
+    var inventory = [];
 
-  return {
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
+    return {
+        getInventory: function() {
+            return inventory;
+        },
 
-      inventoryLoader.addEventListener("load", function () {
 
-      });
-    }
-  };
+        loadInventory: function(callback) {
+            var inventoryLoader = new XMLHttpRequest();
+
+            inventoryLoader.addEventListener("load", loadSuccess);
+            inventoryLoader.addEventListener("failed", loadFail);
+
+            inventoryLoader.open("GET", "inventory.json");
+            inventoryLoader.send();
+
+            function loadFail() {
+                console.log("Unable to load data.")
+            };
+
+            function loadSuccess() {
+                inventory = JSON.parse(this.responseText).cars;
+                callback();
+            };
+
+        }
+    };
 
 })();
+
+//I'll put comments in here when finished
--- quiz.js
+++ quiz.js
@@ -1,10 +1,40 @@
-function populatePage (inventory) {
-  // Loop over the inventory and populate the page
+function populatePage(inventory) {
+//3 starter variables, not to be confused with starter pokemon
+    var htmlContainer = document.getElementById('container');
+    var sold = "sold";
+    var nissanString;
 
-  // Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
-}
+    for (var i = 0; i < inventory.length; i++) {
 
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-CarLot.loadInventory();
+
+            nissanString = `<div class="col-sm-4 car`;
+            if (inventory[i].purchased) {
+                carString += sold;
+            }
+            nissanString += `" style="border-color: ${inventory[i].color};">`;
+            nissanString += `<h1>${inventory[i].make}</h1>`;
+            nissanString += `<h2>${inventory[i].model}</h2>`;
+            nissanString += `<h2>Year: ${inventory[i].year}</h2>`;
+            nissanString += `<h2>Price: $${inventory[i].price}</h2>`;
+            nissanString += `<p>${inventory[i].description}</p>`;
+            //if for inventory whether it has been purchased or not 
+            if (inventory[i].purchased) {
+                nissanString += `<img class="soldImage" src="sold.jpg">`;
+            }
+            //will get actual picture soon
+            nissanString += `<img class="questionImage" src="Question.png">`;
+            nissanString += `</div>`;
+            htmlContainer.innerHTML += nissanString;
+
+    };
+
+};
+//load the inventory to the dom
+CarLot.loadInventory(carsToDOM);
+
+function carsToDOM() {
+    var carInventory = CarLot.getInventory();
+    populatePage(carInventory)
+};
+
+//I'll put comments in here when finished
